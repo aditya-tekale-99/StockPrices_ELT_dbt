2fa3745316a67d9233426ca5aee8046bf94584ea
Adi <84354252+aditya-tekale-99@users.noreply.github.com>
2024-11-03 13:25:20 -0800

    Add files via upload

---
+++ stock_prediction/models/tranformations/bollinger.sql
@@ -0,0 +1,27 @@
+WITH bollinger AS (
+    SELECT
+        symbol,
+        date,
+        close,
+        AVG(close) OVER (
+            PARTITION BY symbol 
+            ORDER BY date 
+            ROWS BETWEEN 19 PRECEDING AND CURRENT ROW
+        ) AS sma_20d,
+        STDDEV(close) OVER (
+            PARTITION BY symbol 
+            ORDER BY date 
+            ROWS BETWEEN 19 PRECEDING AND CURRENT ROW
+        ) AS stddev_20d
+    FROM {{ ref('stock_prices') }}
+)
+
+SELECT
+    symbol,
+    date,
+    close,
+    COALESCE(sma_20d, 0) AS sma_20d,
+    COALESCE(sma_20d + (2 * stddev_20d), 0) AS upper_band,
+    COALESCE(sma_20d - (2 * stddev_20d), 0) AS lower_band
+FROM bollinger
+ORDER BY symbol, date
