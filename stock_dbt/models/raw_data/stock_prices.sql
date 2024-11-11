d5f6c270bb08e5553213532049179cff6f894492
Adi <84354252+aditya-tekale-99@users.noreply.github.com>
2024-11-11 12:04:11 -0800

    config: added CTE query

--- stock_dbt/models/raw_data/stock_prices.sql
+++ stock_dbt/models/raw_data/stock_prices.sql
@@ -1,9 +1,13 @@
-SELECT
-    symbol,
-    date,
-    open,
-    high,
-    low,
-    close,
-    volume
-FROM {{ source('raw_data', 'stock_prices') }}
+WITH stock_data AS (
+    SELECT
+        symbol,
+        date,
+        open,
+        high,
+        low,
+        close,
+        volume
+    FROM {{ source('raw_data', 'stock_prices') }}
+)
+
+SELECT * FROM stock_data;
