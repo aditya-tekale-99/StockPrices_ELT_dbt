20a286aabe80e78b2a03053ce787e7b17287517f
Adi <84354252+aditya-tekale-99@users.noreply.github.com>
2024-11-12 17:09:20 -0800

    cleanup: made query prettier

--- stock_dbt/models/tranformations/rsi_calculation.sql
+++ stock_dbt/models/tranformations/rsi_calculation.sql
@@ -28,13 +28,11 @@ average_gains_losses AS (
         symbol,
         date,
         close,
-        -- Calculate the average gain over the last 14 days, including the current row
         AVG(gain) OVER (
             PARTITION BY symbol 
             ORDER BY date 
             ROWS BETWEEN 13 PRECEDING AND CURRENT ROW
         ) AS avg_gain,
-        -- Calculate the average loss over the last 14 days, including the current row
         AVG(loss) OVER (
             PARTITION BY symbol 
             ORDER BY date 
@@ -49,7 +47,6 @@ SELECT
     close,
     avg_gain,
     avg_loss,
-    -- Calculate RSI, using COALESCE to set to 0 if result is NULL
     COALESCE(100 - (100 / (1 + (NULLIF(avg_gain, 0) / NULLIF(avg_loss, 0)))), 0) AS rsi_14d
 FROM average_gains_losses
 ORDER BY symbol, date
