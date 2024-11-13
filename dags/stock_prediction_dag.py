67e9adc92d3ef90498f7669ada96cfd24f94241e
Adi <84354252+aditya-tekale-99@users.noreply.github.com>
2024-11-13 09:09:48 -0800

    bugfix: dag only pulls 90 records for each symbol instead of 180

--- dags/stock_prediction_dag.py
+++ dags/stock_prediction_dag.py
@@ -1,119 +1,109 @@
-#importing necessary libraries
 from airflow import DAG
 from airflow.models import Variable
 from airflow.decorators import task
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+from datetime import timedelta
 from datetime import datetime
+import snowflake.connector
 import requests
-import logging
 
-#setting up snowflake connection
+
+#for initiating snowflake connection
 def return_snowflake_conn():
-    hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+
+    hook = SnowflakeHook(snowflake_conn_id='snowflake_conn') # Initialize the SnowflakeHook
+
+    # Execute the query and fetch results
     conn = hook.get_conn()
-    return conn
+    return conn.cursor()
 
-#task to extract data from alphavantage 
+#extracting stock data
 @task
-def extract_stock_data():
-    api_key = Variable.get("alpha_vantage_api_key")
-    symbols = ["TTWO", "GOOGL"]
-    stock_data = {}  # Empty dictionary
-
+def extract(vantage_api_key, symbols):
+    data = {}
     for symbol in symbols:
-        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}'
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={vantage_api_key}'
         response = requests.get(url)
-        data = response.json()
+        stock_data = response.json().get("Time Series (Daily)", {})
+
+        #Sorting dates in descending order and taking the first 90 entries
+        last_90_days = dict(list(stock_data.items())[:90])
         
-        if "Time Series (Daily)" in data:
-            daily_data = data["Time Series (Daily)"]
-            latest_90_days_data = dict(list(daily_data.items())[:90])
-            stock_data[symbol] = latest_90_days_data
-        else:
-            logging.error(f"No data for {symbol}: {data}")
-    
-    logging.info(stock_data)
-    return stock_data
+        data[symbol] = last_90_days #storing only the last 90 days for each stock symbol
+    return data
 
-#task to transform the extracted data
+#transforming stock data
 @task
-def transform_stock_data(raw_data):
-    transformed_data = [] #empty list
+def transform(data, symbols):
+    results = []
+    for symbol in symbols:
+        for date, stock_info in data[symbol].items():
+            stock_info["date"] = date
+            stock_info["symbol"] = symbol  # Adding the symbol dynamically
+            results.append(stock_info)
+    return results
 
-    for symbol, daily_data in raw_data.items():
-        if not daily_data:
-            logging.warning(f"No daily data for {symbol}.")
-            continue
-
-        for date, price_info in daily_data.items():
-            transformed_data.append({
-                'symbol': symbol,
-                'date': date,
-                'open': price_info['1. open'],
-                'high': price_info['2. high'],
-                'low': price_info['3. low'],
-                'close': price_info['4. close'],
-                'volume': price_info['5. volume']
-            })
-    
-    logging.info(transformed_data)
-    return transformed_data[:180]
-
-#task to load transformed data into snowflake table
+#loading the stock data in Snowflake
 @task
-def load_to_snowflake(data):
-    conn = return_snowflake_conn() 
-    cur = conn.cursor() 
+def load_to_snowflake(records, target_table):
+    cur = return_snowflake_conn()
     try:
         cur.execute("BEGIN;")
         cur.execute("CREATE DATABASE IF NOT EXISTS dev;")
         cur.execute("USE DATABASE dev;")
-        cur.execute("CREATE SCHEMA IF NOT EXISTS raw_data;")
-        cur.execute("USE SCHEMA raw_data;")        
-        cur.execute("""
-            CREATE OR REPLACE TABLE raw_data.stock_prices (
-                symbol VARCHAR(10),
-                date timestamp_ntz,
-                open FLOAT,
-                high FLOAT,
-                low FLOAT,
-                close FLOAT,
-                volume FLOAT,
-                PRIMARY KEY (symbol, date)
+        cur.execute("CREATE SCHEMA raw_data;")
+        cur.execute("CREATE SCHEMA analytics;")
+        cur.execute("USE SCHEMA raw_data;")
+        cur.execute(f"""
+            CREATE OR REPLACE TABLE {target_table} (
+                date timestamp_ntz primary key,
+                open float,
+                high float,
+                low float,
+                close float,
+                volume bigint,
+                symbol varchar
             );
         """)
-        
-        for record in data:
-            try:
-                sql = f"""
-                    INSERT INTO dev.raw_data.stock_prices (symbol, date, open, high, low, close, volume)
-                    VALUES ('{record['symbol']}', '{record['date']}', {record['open']}, {record['high']}, {record['low']}, {record['close']}, {record['volume']});
-                """
-                cur.execute(sql)
-            except Exception as e:
-                logging.error(f"Failed to insert record for {record['symbol']} on {record['date']}: {e}")
+        for r in records:
+            date = r['date']
+            open_price = r['1. open']
+            high_price = r['2. high']
+            low_price = r['3. low']
+            close_price = r['4. close']
+            volume = r['5. volume']
+            symbol = r['symbol']
+
+            sql = f"""
+                INSERT INTO {target_table} (date, open, high, low, close, volume, symbol)
+                VALUES (TO_TIMESTAMP_NTZ('{date}', 'YYYY-MM-DD'), {open_price}, {high_price}, {low_price}, {close_price}, {volume}, '{symbol}')
+            """
+            cur.execute(sql)
 
         cur.execute("COMMIT;")
-        
-    except Exception as e:
-        logging.error(f"Error occurred during loading to Snowflake: {e}")
-        cur.execute("ROLLBACK;")
-        raise e
-    finally:
-        cur.close() 
-        conn.close()
 
-#dag information
+    except Exception as e:
+        cur.execute("ROLLBACK;")
+        print(e)
+        raise e
+
+    finally:
+        cur.close()
+
 with DAG(
-    dag_id='stock_prediction_model_v1.2',
-    start_date=datetime(2024, 11, 11),
-    schedule_interval='@daily',
+    dag_id = 'stock_prediction_model_v1.2',
+    start_date = datetime(2024,11,11),
     catchup=False,
-    tags=['stock_prices', 'ETL', 'TTWO', 'GOOGL', 'Analytics'] #tags to easily identify the dag in airflow
+    tags=['stock_prices','ETL', 'GOOGL', 'TTWO'],
+    schedule = '@daily'
 ) as dag:
 
-    raw_data = extract_stock_data()
-    transformed_data = transform_stock_data(raw_data)
-    load_data = load_to_snowflake(transformed_data)
+    target_table = 'dev.raw_data.stock_prices'
+    vantage_api_key = Variable.get('alpha_vantage_api_key')
+    symbols = ['GOOGL', 'TTWO']
 
-    raw_data >> transformed_data >> load_data
+    #tasks
+    data = extract(vantage_api_key, symbols)
+    transformed_data = transform(data, symbols)
+    load_to_snowflake(transformed_data, target_table)
